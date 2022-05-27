d664aa09959f656e5678560dfd635e3cf3d23622
Lucian Miller <lucian.miller185@gmail.com>
2022-05-27 10:02:22 -0700

    Add basic test set up

---
+++ Calendar.Solution/CalendarTests/ModelsTests/LeapYearTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.Test.Tools.UnitTesting;
+using Calendar;
+
+namespace CalendarTests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    // test method will go here
+  }
+}
