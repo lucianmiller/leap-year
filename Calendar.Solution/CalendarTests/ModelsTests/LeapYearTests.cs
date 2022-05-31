735dedfa8bd851e47e79279632e04ab8d62af2fc
Lucian Miller <lucian.miller185@gmail.com>
2022-05-31 10:33:14 -0700

    Add tests and solutions

--- Calendar.Solution/CalendarTests/ModelsTests/LeapYearTests.cs
+++ Calendar.Solution/CalendarTests/ModelsTests/LeapYearTests.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualStudio.Test.Tools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Calendar;
 
 namespace CalendarTests
@@ -7,6 +7,24 @@ namespace CalendarTests
   public class LeapYearTests
   {
     [TestMethod]
-    // test method will go here
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear(1999));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear(1900));
+    }
   }
 }
